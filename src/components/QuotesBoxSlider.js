d448dddb07339a78ca10f0e807027e367fd481c0
Mahalakshmi Karmegam <mahalakshmi.karmegam@ting.in>
2024-08-23 14:52:18 +0530

    slider

--- src/components/QuotesBoxSlider.js
+++ src/components/QuotesBoxSlider.js
@@ -1,36 +1,46 @@
+//Component
 import QuoteBox from "./QuoteBox";
 import Slider from "react-slick";
 import Image from "next/image";
+
+// STYLES
 import styles from "@/styles/components/QuotesBoxSlider.module.scss";
+
+//images
 import logo1 from "@/../public/img/assets/LogoAnimation/img1.png";
 import logo2 from "@/../public/img/assets/LogoAnimation/img2.png";
 import logo3 from "@/../public/img/assets/LogoAnimation/img3.png";
+import Quotes from "@/../public/img/assets/Quotes.png";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import DefaultBanner from "@/../public/img/banner/defaultDesktopBanner.jpg";
+
 /** QuotesBoxSlider Component */
 export default function QuotesBoxSlider() {
 	const data = [
 		{
+			quotesImg: Quotes,
 			id: 1,
-			img: { logo1 },
+			img: logo1,
 			company: "Business head, Zee Tv",
 			name: "Parul Goel",
 			quotes:
 				"I've partnered with Nidhi since 2015 for various successful projects, including an outdoor launch for & TV. Mango Marketing & Events epitomizes approachability, flexibility, and dedication. They prioritize prompt, customer-first solutions, making collaboration seamless. Nidhi's proactive approach and collaborative working style have been a pleasure to work with.",
 		},
 		{
+			quotesImg: Quotes,
 			id: 2,
-			img: { logo2 },
+			img: logo2,
 			company: "Business head, Zee Tv",
 			name: "Parul Goel",
 			quotes:
 				"I've partnered with Nidhi since 2015 for various successful projects, including an outdoor launch for & TV. Mango Marketing & Events epitomizes approachability, flexibility, and dedication. They prioritize prompt, customer-first solutions, making collaboration seamless. Nidhi's proactive approach and collaborative working style have been a pleasure to work with.",
 		},
 		{
+			quotesImg: Quotes,
+
 			id: 3,
-			img: { logo3 },
+			img: logo3,
 			company: "Business head, Zee Tv",
 			name: "Parul Goel",
 			quotes:
@@ -42,8 +52,8 @@ export default function QuotesBoxSlider() {
 
 	const settings = {
 		dots: true,
-		
-		infinite: true,
+
+		// infinite: true,
 		speed: 300,
 		slidesToShow: 3,
 		slidesToScroll: 1,
@@ -51,14 +61,22 @@ export default function QuotesBoxSlider() {
 	};
 
 	return (
-		<div className={`${styles.QuoteBox}`}>
-			<Slider className="container" {...settings}>
+		<div className={`${styles.QuoteBox} section_spacing`}>
+			<Slider {...settings} className={`${styles.Slider}`}>
 				{data.map((item) => (
-					<div  key={item.id}>
-						<p>{item.company}</p>
-						<p>{item.name}</p>
-						<p>{item.quotes}</p>
-						<img src={item.img} alt="" />
+					<div key={item.id} className={`${styles.Flex}`}>
+						<Image src={item.quotesImg} width={39} alt="" />
+						<div><p>{item.quotes}</p></div>
+						
+						<div>
+							<div className={`${styles.flexClm}`}>
+								{" "}
+								<p>{item.name}</p>
+								<p>{item.company}</p>
+							</div>
+
+							<Image src={item.img} width={75} height={40} alt="" />
+						</div>
 					</div>
 				))}
 			</Slider>
